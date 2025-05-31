69b8450017bd1712edbfaba8f63244843bb5d0ea
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-31 19:10:56 -0300

    Add add jest settings

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios|framer-motion|react-router-dom)'
+  ],
+};
